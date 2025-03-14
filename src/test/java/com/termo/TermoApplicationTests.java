2fa7b91c75f43ec72fe5ff30a8f467ac11b054c5
o-gustavolopes <gustavolopesmartins2506@gmail.com>
2025-03-14 12:36:49 -0300

    Project init

---
+++ src/test/java/com/termo/TermoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.termo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
